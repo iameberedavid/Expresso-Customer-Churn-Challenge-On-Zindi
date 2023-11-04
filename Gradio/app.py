7850a978e9be2de014c95c33ac98e22925860e58
iameberedavid <eberedavid326@gmail.com>
2023-11-04 02:55:52 +0100

    Gradio pp

--- Gradio/app.py
+++ Gradio/app.py
@@ -1,34 +1,30 @@
-import streamlit as st
+# Load key libraries
+import gradio as gr
 import pandas as pd
 import numpy as np
-import os
 import pickle
+import os
+import catboost
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
-# Useful functions
-def load_ml_components(fp):
-    'Load the ML component to re-use in app'
-    with open(fp, 'rb') as f:
-        object = pickle.load(f)
-        return object
 
-# Variables and Constants
-DIRPATH = os.path.dirname(os.path.realpath(__file__))
-ml_core_fp = os.path.join(DIRPATH, 'Assets', 'Export', 'ml_components.pkl')
+def predict_churn(TENURE, MONTANT, FREQUENCE_RECH, REVENUE, ARPU_SEGMENT, FREQUENCE, DATA_VOLUME, ON_NET,
+                  ORANGE, TIGO, REGULARITY, FREQ_TOP_PACK):
 
-# Load the Machine Learning components
-ml_components_dict = load_ml_components(fp=ml_core_fp)
+    # Load the ML components
+    DIRPATH = os.path.dirname(os.path.realpath(__file__))
+    ml_core_fp = os.path.join(DIRPATH, 'ml_components.pkl')
+    with open(ml_core_fp, "rb") as f:
+        loaded_model_components = pickle.load(f)
+    
+    # Extract the ML components
+    imputer = ml_components_dict['imputer']
+    scaler = ml_components_dict['scaler']
+    encoder = ml_components_dict['encoder']
+    model = ml_components_dict['model']
 
-# Extract the ML components
-imputer = ml_components_dict['imputer']
-scaler = ml_components_dict['scaler']
-encoder = ml_components_dict['encoder']
-best_model = ml_components_dict['model']
-
-# Preprocess the data
-def preprocess_data(df, imputer, encoder, scaler):
     numerical_features = ['MONTANT', 'FREQUENCE_RECH', 'REVENUE', 'ARPU_SEGMENT', 'FREQUENCE', 'DATA_VOLUME',
                           'ON_NET', 'ORANGE', 'TIGO', 'REGULARITY', 'FREQ_TOP_PACK']
     categorical_features = ['TENURE']
@@ -55,72 +51,32 @@ def preprocess_data(df, imputer, encoder, scaler):
 
     # Convert the NumPy array to a pandas DataFrame
     processed_df = pd.DataFrame(processed_df, columns=all_feature_names)
+    
+    # Make prediction using the loaded model
+    prediction = model.predict_proba(processed_df)
+    prob_churn = float(prediction[0][1])
+    return {'Churn Probability': prob_churn}
 
-    st.write('Columns in the DataFrame:', processed_df.columns.tolist())
-    st.write('DataFrame Shape:', processed_df.shape)
+# Define the inputs
+TENURE = gr.Radio(choices=['K > 24 month', 'E 6-9 month', 'H 15-18 month', 'G 12-15 month',
+                           'I 18-21 month', 'J 21-24 month', 'F 9-12 month', 'D 3-6 month'], label='TENURE')
+MONTANT = gr.Number(label='MONTANT')
+FREQUENCE_RECH = gr.Number(label='FREQUENCE_RECH')
+REVENUE = gr.Number(label='REVENUE')
+ARPU_SEGMENT = gr.Number(label='ARPU_SEGMENT')
+FREQUENCE = gr.Number(label='FREQUENCE')
+DATA_VOLUME = gr.Number(label='DATA_VOLUME')
+ON_NET = gr.Number(label='ON_NET')
+ORANGE = gr.Number(label='ORANGE')
+TIGO = gr.Number(label='TIGO')
+REGULARITY = gr.Number(label='REGULARITY')
+FREQ_TOP_PACK = gr.Number(label='FREQ_TOP_PACK')
 
-    return processed_df
-
-def main():
-    st.set_page_config(page_title='Customer Churn Prediction App', page_icon='📊', layout='wide')
-    st.title('Customer Churn Prediction App')
-    st.markdown(
-    """
-<style>
-    body {
-        background-color: #008080;  /* Blue background color */
-        color: #000000;  /* Text color */
-    }
-</style>
-    """,
-    unsafe_allow_html=True
-)
-
-
-    # User input section
-    TENURE = st.selectbox('TENURE', ['K > 24 month', 'E 6-9 month', 'H 15-18 month', 'G 12-15 month',
-                                     'I 18-21 month', 'J 21-24 month', 'F 9-12 month', 'D 3-6 month'])
-    MONTANT = st.number_input('MONTANT', min_value=0, step=1)
-    FREQUENCE_RECH = st.number_input('FREQUENCE_RECH', min_value=0, step=1)
-    REVENUE = st.number_input('REVENUE', min_value=0, step=1)
-    ARPU_SEGMENT = st.number_input('Transactions', min_value=0, step=1)
-    FREQUENCE = st.number_input('FREQUENCE', min_value=0, step=1)
-    DATA_VOLUME = st.number_input('DATA_VOLUME', min_value=0, step=1)
-    ON_NET = st.number_input('ON_NET', min_value=0, step=1)
-    ORANGE = st.number_input('ORANGE', min_value=0, step=1)
-    TIGO = st.number_input('TIGO', min_value=0, step=1)
-    REGULARITY = st.number_input('REGULARITY', min_value=0, step=1)
-    FREQ_TOP_PACK = st.number_input('FREQ_TOP_PACK', min_value=0, step=1)
-							
-    # Predict button
-    if st.button('Predict Customer Churn'):
-        # Prepare input data for prediction
-        input_data = {
-            'TENURE': [TENURE],
-            'MONTANT': [MONTANT],
-            'FREQUENCE_RECH': [FREQUENCE_RECH],
-            'REVENUE': [REVENUE],
-            'ARPU_SEGMENT': [ARPU_SEGMENT],
-            'FREQUENCE': [FREQUENCE],
-            'DATA_VOLUME': [DATA_VOLUME],
-            'ON_NET': [ON_NET],
-            'ORANGE': [ORANGE],
-            'TIGO': [TIGO],
-            'REGULARITY': [REGULARITY],
-            'FREQ_TOP_PACK': [FREQ_TOP_PACK],
-        }
-
-        df = pd.DataFrame(input_data)
-
-        # Preprocess the input data
-        df = preprocess_data(df, imputer, scaler, encoder)
-
-        # Make predictions using the loaded best model
-        prediction = best_model.predict_proba(df)
-        prob_churn = float(prediction[0][1])
-
-        # Display the prediction
-        st.write(f'Customer Churn Probability: {prob_churn}')
-
-if __name__ == '__main__':
-    main()
+# Design the interface
+gr.Interface(inputs=[TENURE, MONTANT, FREQUENCE_RECH, REVENUE, ARPU_SEGMENT, FREQUENCE, DATA_VOLUME, ON_NET,
+                     ORANGE, TIGO, REGULARITY, FREQ_TOP_PACK],
+    outputs=gr.Label('Awaiting Submission...'),
+    fn=predict_churn,
+    title='Customer Churn Prediction App',
+    description="This app predicts whether a telecommunication network's customer will churn or not based on available data."
+).launch(inbrowser=True, show_error=True, share=True)
